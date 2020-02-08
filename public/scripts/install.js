cccaceb192d022bf8b86597ce87320b16a151175
Rodrigo Porcionato Allves <rodrigoporcionato@gmail.com>
2020-02-08 14:55:27 -0300

    add pwa site

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -1,27 +1,10 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
 'use strict';
 
 let deferredInstallPrompt = null;
+
 const installButton = document.getElementById('butInstall');
 installButton.addEventListener('click', installPWA);
 
-// CODELAB: Add event listener for beforeinstallprompt event
 
 
 /**
@@ -32,7 +15,8 @@ installButton.addEventListener('click', installPWA);
  */
 function saveBeforeInstallPromptEvent(evt) {
   // CODELAB: Add code to save event & show the install button.
-
+  deferredInstallPrompt = evt;
+  installButton.removeAttribute('hidden');
 }
 
 
@@ -43,12 +27,22 @@ function saveBeforeInstallPromptEvent(evt) {
  */
 function installPWA(evt) {
   // CODELAB: Add code show install prompt & hide the install button.
-
+  deferredInstallPrompt.prompt();
+  // Hide the install button, it can't be called twice.
+  evt.srcElement.setAttribute('hidden', true);
   // CODELAB: Log user response to prompt.
-
+  deferredInstallPrompt.userChoice
+  .then((choice) => {
+    if (choice.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt', choice);
+    } else {
+      console.log('User dismissed the A2HS prompt', choice);
+    }
+    deferredInstallPrompt = null;
+  });
 }
 
-// CODELAB: Add event listener for appinstalled event
+
 
 /**
  * Event handler for appinstalled event.
@@ -58,5 +52,11 @@ function installPWA(evt) {
  */
 function logAppInstalled(evt) {
   // CODELAB: Add code to log the event
-
+  console.log('Weather App was installed.', evt);
 }
+
+
+// CODELAB: Add event listener for appinstalled event
+window.addEventListener('appinstalled', logAppInstalled);
+// CODELAB: Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
